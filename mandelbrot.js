49da8dc22e44400fde7f7652bbf97347fb4d866c
danielJin100 <75294070+danielJin100@users.noreply.github.com>
2022-01-27 17:37:35 -0800

    Add files via upload
    takes a lot less time to run now

--- mandelbrot.js
+++ mandelbrot.js
@@ -3,9 +3,9 @@ function mandelbrot(posx, posy, iterations, max){
     var z = {x: 0, y:0};
     var i = 1;
     while(z.x < max && z.y < max && i < iterations){
-      z = {x: (z.x*z.x-z.y*z.y)+c.x,y: (2*z.x*z.y)+c.y};
+      z = {x: (Math.pow(z.x, 2)-Math.pow(z.y, 2))+c.x,y: (2*z.x*z.y)+c.y};
       if(z.x >= max || z.y >= max){
-        return false;
+        return i;
       }
       i++;
     }
@@ -16,18 +16,17 @@ function mandelbrot(posx, posy, iterations, max){
   
 function setup(){
   createCanvas(1000, 400);
-  for(var c = 1; c <= 500; c++){
-    if(c === 500){
-        stroke(0);
-    } else {
-        stroke(c+100, 1.5*c, 1.5*c); 
-    }
+  
     for(var a = 0; a < width; a++){
+        
         for(var b = 0; b < height; b++){
-            if(mandelbrot((a-width/2)/100, (b-height/2)/100, c, c)){
-                point(a, b);
+          x = mandelbrot((a-width/2)/500, (b-height/2)/500, 500, 500)
+            if(x == true){
+              stroke(0);
+            } else {
+              stroke(x+100, 1.5*x, 1.5*x); 
             }
-        }
+           point(a, b);
+          }
     }
-}
 }
